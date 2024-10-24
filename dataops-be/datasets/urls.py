2fe4d8074c0bb0a6fd84d05613903ee40092e41c
kyj7479 <kyj7479@diquest.com>
2024-10-24 02:15:38 +0000

    [DBE] init other datasets API

--- dataops-be/datasets/urls.py
+++ dataops-be/datasets/urls.py
@@ -3,4 +3,7 @@ from datasets import views
 
 urlpatterns = [
     path('datasets', views.DatasetList.as_view()),
+    path('datasets/<int:dataset_id>/', views.DatasetUpload.as_view()),
+    path('datasets/<int:dataset_id>/analysis/', views.DatasetAnalysis.as_view()),
+    path('datasets/<int:dataset_id>/process/', views.DatasetProcess.as_view()),
 ]
