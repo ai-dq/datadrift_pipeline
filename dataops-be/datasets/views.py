83d26b0552959ff2771ef3e6976e4071659cfa77
kyj7479 <kyj7479@diquest.com>
2024-10-24 02:15:38 +0000

    [DBE] init other datasets API

--- dataops-be/datasets/views.py
+++ dataops-be/datasets/views.py
@@ -9,4 +9,18 @@ class DatasetList(APIView):
         
     def get(self, request):
         return Response({"message": "Dataset example."}, status=status.HTTP_200_OK)
+    
+# /datasets/{dataset_id} (학습 데이터 업로드)
+class DatasetUpload(APIView):
+    def post(self, request, dataset_id):
+        return Response({"message": "Data uploaded."}, status=status.HTTP_201_CREATED)
 
+# /datasets/{dataset_id}/analysis (드리프트 검출 요청)
+class DatasetAnalysis(APIView):
+    def post(self, request, dataset_id):
+        return Response({"message": "Dataset analysis started."}, status=status.HTTP_200_OK)
+
+# /datasets/{dataset_id}/process (데이터셋 가공)
+class DatasetProcess(APIView):
+    def post(self, request, dataset_id):
+        return Response({"message": "Dataset processing started."}, status=status.HTTP_200_OK)
