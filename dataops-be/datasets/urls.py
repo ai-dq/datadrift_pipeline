5fa89bcb5d0c28d97133c6b1b08c0707b91b12f9
kyj7479 <kyj7479@diquest.com>
2025-03-31 01:19:58 +0000

    [DBE] feat: 전체 및 단일 조회용 GET /datasets API 초기 구현

--- dataops-be/datasets/urls.py
+++ dataops-be/datasets/urls.py
@@ -3,9 +3,9 @@ from datasets import views
 
 urlpatterns = [
     path('datasets', views.DatasetList.as_view()),
-    path('datasets/<int:dataset_id>', views.DatasetUpload.as_view()),
-    path('datasets/<int:dataset_id>/analysis', views.DatasetAnalysis.as_view()),
-    path('datasets/<int:dataset_id>/process', views.DatasetProcess.as_view()),
-    path('datasets/<int:dataset_id>/files', views.DatasetFileList.as_view()),
-    path('datasets/<int:dataset_id>/files/<int:file_id>', views.DatasetFileDetail.as_view()),
+    path('datasets/<str:dataset_id>', views.DatasetUpload.as_view()),
+    path('datasets/<str:dataset_id>/analysis', views.DatasetAnalysis.as_view()),
+    path('datasets/<str:dataset_id>/process', views.DatasetProcess.as_view()),
+    path('datasets/<str:dataset_id>/files', views.DatasetFileList.as_view()),
+    path('datasets/<str:dataset_id>/files/<int:file_id>', views.DatasetFileDetail.as_view()),
 ]
