b720703d2dccfb368ff3890a848df7996361b316
kyj7479 <kyj7479@diquest.com>
2025-04-22 01:59:26 +0000

    [DBE] docs: api_spec.yaml 수정, swagger 자동 생성 아닌 문서 반영 수정

--- dataops-be/dataops_be/urls.py
+++ dataops-be/dataops_be/urls.py
@@ -1,22 +1,14 @@
 # urls.py
+from django.conf import settings
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="DataOps API",
-      default_version='v1',
-      description="API documentation",
-      terms_of_service="https://www.google.com/policies/terms/",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+from django.urls import re_path
+from django.views.static import serve
+import os
 
 urlpatterns = [
-    path('', include('datasets.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger/(?P<path>.*)$', serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'static/openapi/swagger-dist-ui'),
+        'show_indexes': True,
+    }),
+   path('', include('datasets.urls')),
 ]
