7798b2ce5689bcd87503f46812e98ad2e01a8dc6
kyj7479 <kyj7479@diquest.com>
2025-04-22 02:00:56 +0000

    [DFE] feat: 프론트엔드 dataset 조회 최초 구현

--- dataops-fe/vue.config.js
+++ dataops-fe/vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// WebServer 배포할 경우 사용
+// module.exports = {
+//   devServer: {
+//     host: '121.126.210.15',
+//     port: 8081,
+//     proxy: {
+//       '^/': {
+//         target: 'http://localhost:9466',
+//         changeOrigin: true,
+//         ws: true
+//       }
+//     }
+//   }
+// }
