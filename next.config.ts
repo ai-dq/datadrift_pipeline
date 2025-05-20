d8a34531fab4e2de464f66253de0c3a0b3753a34
이창준 <junnos41@diquest.com>
2025-05-20 15:46:50 +0900

    Initial commit from Create Next App

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    'local-origin.dev',
-    '*.local-origin.dev',
-    '121.126.210.2',
-  ],
-  rewrites: async () => {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: `http://121.126.210.2:${process.env.CORE_API_PORT}/:path*`,
-        },
-      ];
-    } else {
-      return [
-        {
-          source: '/api/:path*',
-          destination: `http://localhost:${process.env.CORE_API_PORT}/:path*`,
-        },
-      ];
-    }
-  },
+  /* config options here */
 };
 
 export default nextConfig;
