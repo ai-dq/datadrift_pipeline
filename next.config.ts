149f4d8cebcafc79bef42ddcade6b3e5706577a6
이창준 <junnos41@diquest.com>
2025-06-12 05:30:05 +0000

    🐛 Fix model list page sending request forever

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '121.126.210.2'],
+  allowedDevOrigins: [
+    'local-origin.dev',
+    '*.local-origin.dev',
+    '121.126.210.2',
+  ],
 };
 
 export default nextConfig;
