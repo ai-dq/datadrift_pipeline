a9661a6a2b1a465c30b7f7e623e6d51dd236d9b7
이창준 <junnos41@diquest.com>
2025-06-13 06:32:01 +0000

    🐛 Fix Next.js API path to 'next-api' from 'api'
    This is for resolving conflict with Docker API container's API path

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -3,7 +3,7 @@ import { GetMLModelsResponse, MLModelResponse } from './models/ml-models';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const NEXT_PUBLIC_API_PROXY_PREFIX = '/api/external';
+const NEXT_PUBLIC_API_PROXY_PREFIX = '/next-api/external';
 const API_BASE_URL = isDevelopment ? 'http://121.126.210.2/api/v1' : '/';
 const effectivePrefix = isDevelopment ? '' : NEXT_PUBLIC_API_PROXY_PREFIX;
 
