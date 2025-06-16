47b6a24f4432c558c3a1d271dc4d2fbc826ab00c
이창준 <junnos41@diquest.com>
2025-06-16 02:12:54 +0000

    ✏️ Fix API rewrite url

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,12 +1,7 @@
 import { useCallback, useState } from 'react';
 import { GetMLModelsResponse, MLModelResponse } from './models/ml-models';
 
-// API configuration
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-const API_BASE_URL = isDevelopment
-  ? 'http://121.126.210.2/api/v1'
-  : '/proxy/api/v1';
+const API_BASE_URL = '/api/v1';
 
 type RequestOptions = {
   method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
