b0aff711d85c69b071e67d508b7c6b9ecdb88c91
이창준 <junnos41@diquest.com>
2025-06-16 06:19:00 +0000

    ✏️ Use env values for services

--- src/app/dashboard/swagger/page.tsx
+++ src/app/dashboard/swagger/page.tsx
@@ -1,7 +1,10 @@
 export default function SwaggerPage() {
   return (
     <div className="w-full h-screen">
-      <iframe src="http://121.126.210.2/api/docs" className="w-full h-screen" />
+      <iframe
+        src={process.env.CORE_API_SWAGGER_URL}
+        className="w-full h-screen"
+      />
     </div>
   );
 }
