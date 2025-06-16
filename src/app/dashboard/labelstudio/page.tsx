8bfcc124087b97ff43238c71ebdfdcafe06ef40e
이창준 <junnos41@diquest.com>
2025-06-16 06:17:29 +0000

    💄 Implement iframe-based service pages

--- src/app/dashboard/labelstudio/page.tsx
+++ src/app/dashboard/labelstudio/page.tsx
@@ -1,7 +1,10 @@
 export default function LabelStudioPage() {
   return (
     <div className="w-full h-screen">
-      <iframe src={process.env.LABEL_STUDIO_URL} className="w-full h-screen" />
+      <iframe
+        src="http://121.126.210.2/label-studio"
+        className="w-full h-screen"
+      />
     </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,3 @@
-export default function Dashboard() {
-  return (
-    <div className="w-full h-screen">
-      <iframe
-        src="http://121.126.210.2/label-studio"
-        className="w-full h-screen"
-      />
-    </div>
-  );
+export default function DashboardPage() {
+  return <div className="w-full h-screen">Hello World!</div>;
 }
