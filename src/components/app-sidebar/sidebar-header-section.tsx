3a50786e1357ce06b308bf7678632378cdfb631e
이창준 <junnos41@diquest.com>
2025-06-16 06:20:46 +0000

    🚨 Fix lint error

--- src/components/app-sidebar/sidebar-header-section.tsx
+++ src/components/app-sidebar/sidebar-header-section.tsx
@@ -5,15 +5,15 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarTrigger,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 export const SidebarHeaderSection = () => (
   <SidebarHeader>
     <SidebarMenu>
       <SidebarMenuItem className="flex items-center justify-between">
         <SidebarMenuButton size="lg" asChild>
-          <a href="/">
+          <Link href="/">
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
               <GalleryVerticalEnd className="size-4" />
             </div>
@@ -21,7 +21,7 @@ export const SidebarHeaderSection = () => (
               <span className="font-medium">Q-OCR</span>
               <span className="font-normal">v0.0.1 🌈</span>
             </div>
-          </a>
+          </Link>
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
