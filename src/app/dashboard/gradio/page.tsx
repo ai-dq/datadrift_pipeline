0100a41b26cd98f2410ca8a6a8b10eb1098053a3
이창준 <junnos41@diquest.com>
2025-06-16 06:19:00 +0000

    ✏️ Use env values for services

--- src/app/dashboard/gradio/page.tsx
+++ src/app/dashboard/gradio/page.tsx
@@ -1,7 +1,7 @@
 export default function GradioPage() {
   return (
     <div className="w-full h-screen">
-      <iframe src="http://121.126.210.2/demo" className="w-full h-screen" />
+      <iframe src={process.env.CORE_DEMO_URL} className="w-full h-screen" />
     </div>
   );
 }
