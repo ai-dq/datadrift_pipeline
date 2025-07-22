261950108e3549f9bc27a7a3677b336fcacd24e2
Junnos  <junnos.dev@gmail.com>
2025-07-22 09:19:35 +0900

    🩹 Add cursor-pointer style to login button

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm({
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full cursor-pointer">
                 Login
               </Button>
             </div>
