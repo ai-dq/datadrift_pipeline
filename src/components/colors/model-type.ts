4245a3d23e3f4778ff58f43db6d25cd319498882
Junnos  <junnos.dev@gmail.com>
2025-07-22 12:59:40 +0900

    💄 Add model type dropdown and update UI
    The changes in this commit include:
    
    1. Adding a model type dropdown to the project edit dialog, allowing users to select the model type for the project.
    2. Updating the UI of the project edit dialog to use the new form components and styling.
    3. Adding a new `background10` property to the `ModelTypeColor` interface and updating the `ModelTypeColors` object to include this new property.

--- src/components/colors/model-type.ts
+++ src/components/colors/model-type.ts
@@ -2,6 +2,7 @@ import { ModelType } from '@/entities/ml-model';
 
 interface ModelTypeColor {
   background: string;
+  background10: string;
   text: string;
   bgHover: string;
 }
@@ -9,26 +10,31 @@ interface ModelTypeColor {
 export const ModelTypeColors: Record<ModelType, ModelTypeColor> = {
   [ModelType.LAYOUT]: {
     background: 'bg-orange-500/90',
+    background10: 'bg-orange-500/10',
     text: 'text-white',
     bgHover: 'hover:bg-orange-500/60',
   },
   [ModelType.OCRCLS]: {
     background: 'bg-sky-500/90',
+    background10: 'bg-sky-500/10',
     text: 'text-white',
     bgHover: 'hover:bg-sky-500/60',
   },
   [ModelType.OCRDET]: {
     background: 'bg-blue-500/90',
+    background10: 'bg-blue-500/10',
     text: 'text-white',
     bgHover: 'hover:bg-blue-500/60',
   },
   [ModelType.OCRREC]: {
     background: 'bg-indigo-500/90',
+    background10: 'bg-indigo-500/10',
     text: 'text-white',
     bgHover: 'hover:bg-indigo-500/60',
   },
   [ModelType.TABREC]: {
     background: 'bg-teal-500/90',
+    background10: 'bg-teal-500/10',
     text: 'text-white',
     bgHover: 'hover:bg-teal-500/60',
   },
