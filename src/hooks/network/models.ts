a68668039ddd959dd61d3f3df0cda3f72cb5ab51
Junnos  <junnos.dev@gmail.com>
2025-08-01 15:59:22 +0900

    ✨ Add error handling for missing model ID
    - Adds error handling for the `useModelVersions` hook when the `modelID` parameter is `undefined`.
    - If the `modelID` is `undefined`, the hook will return an error message instead of making the API request.
    - Reduces the log level for API errors from `error` to `debug` to avoid cluttering the console.

--- src/hooks/network/models.ts
+++ src/hooks/network/models.ts
@@ -1,5 +1,5 @@
-import { getModelById, getModels } from '@/api/endpoints';
-import { MLModelResponse, MLModelVersionResponse } from '@/api/types';
+import { getModelById, getModelVersions, getModels } from '@/api/endpoints';
+import { APIError, MLModelResponse, MLModelVersionResponse } from '@/api/types';
 import { Model, ModelVersion } from '@/entities/ml-model';
 import { useCallback } from 'react';
 import { useApiData } from './shared/useApiData';
@@ -31,10 +31,45 @@ export function useModel(modelID: number): {
 } {
   const fetchFn = useCallback(() => getModelById(modelID), [modelID]);
 
-  return useApiData<MLModelVersionResponse, ModelVersion>({
+  return useApiItem<MLModelResponse, Model>({
+    fetchFn,
+    transformFn: MLModelResponse.toEntity,
+    errorMessage: `Failed to fetch model with ID: ${modelID}`,
+  });
+}
+
+/**
+ * Custom hook for fetching versions of model by ID
+ */
+export function useModelVersions(modelID: number | undefined): {
+  data: ModelVersion[];
+  loading: boolean;
+  error: string | null;
+  refetch: () => Promise<void>;
+} {
+  const shouldFetch = modelID !== undefined;
+
+  const fetchFn = useCallback(() => {
+    if (!shouldFetch) {
+      return Promise.reject(new APIError(404, 'Model ID is required'));
+    }
+    return getModelVersions(modelID);
+  }, [shouldFetch, modelID]);
+
+  const { data, loading, error, refetch } = useApiData<
+    MLModelVersionResponse,
+    ModelVersion
+  >({
     fetchFn,
     transformFn: MLModelVersionResponse.toEntity,
     errorMessage: `Failed to fetch model with ID: ${modelID}`,
     deps: [modelID],
   });
+
+  return {
+    data,
+    loading,
+    error: shouldFetch ? error : '',
+    refetch: shouldFetch ? refetch : async () => {},
+  };
 }
