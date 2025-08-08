2ce6a6a1b25564544e22ace3e2c381b73c44ab59
Junnos  <junnos.dev@gmail.com>
2025-08-08 09:57:47 +0900

    ➕ Install prerequisites for jest in Next.js

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,11 +2,34 @@
  * Jest setup file for SSE tests
  */
 
-// Mock TextEncoder/TextDecoder for Node.js environment
+// Mock TextEncoder/TextDecoder for Node.js environment first
 if (typeof TextEncoder === 'undefined') {
   const { TextEncoder, TextDecoder } = require('util');
   global.TextEncoder = TextEncoder;
   global.TextDecoder = TextDecoder;
+}
+
+// Mock ReadableStream and MessagePort for undici compatibility
+if (typeof ReadableStream === 'undefined') {
+  global.ReadableStream = require('stream/web').ReadableStream;
+}
+
+if (typeof MessagePort === 'undefined') {
+  global.MessagePort = class MessagePort {
+    start() {}
+    close() {}
+    postMessage() {}
+  };
+}
+
+// Add fetch polyfill for Node.js environment
+// This will be overridden by Jest mocks in unit tests, but needed for integration tests
+if (typeof fetch === 'undefined') {
+  const { fetch, Headers, Request, Response } = require('undici');
+  global.fetch = fetch;
+  global.Headers = Headers;
+  global.Request = Request;
+  global.Response = Response;
 }
 
 // Mock DOMException for abort errors
